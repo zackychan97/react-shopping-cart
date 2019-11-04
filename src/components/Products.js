2d031693b74fa4ebd55eaa6100c9b4ac6e74dcc8
Zack Bumpous <upwork.ztb@gmail.com>
2019-11-04 15:13:32 -0600

    Completes step 4

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,26 @@
-import React from 'react';
+//Step 4A -- import our useContext as well as productContext
+import React, { useContext } from 'react';
+
 
 // Components
 import Product from './Product';
+//Step 4A -- import our useContext as well as productContext
+import { ProductContext } from '../contexts/ProductContext';
 
-const Products = props => {
+//Step 4C remove props from function parameter and replace with ()
+const Products = () => {
+	//Step 4B -- In the component, call the `useContext` hook and pass in the context object we want to use into it.
+	// ProductContext is the context object we want to use into it
+	const {products, addItem} = useContext(ProductContext)
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+		{/*Step 4C get rid of props on props.product.map (destructuring) */}
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					//Step 4C get rid of props on props.addItem (destructuring)
+					addItem={addItem}
 				/>
 			))}
 		</div>
