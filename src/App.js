f1e8db8c79ef3a430b768b017732a778692155a0
Zack Bumpous <upwork.ztb@gmail.com>
2019-11-04 14:26:42 -0600

    Added code into the app.js file to add the given item to the cart, which is step 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
-// Components
+// 	Components (note we have 3) and inside of their files, they all have
+// 	to be passed one of our two states (products and cart).
+//	Imagine how hard this would be to work with as we add more props
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
+	//1st state property products keeps track of products
 	const [products] = useState(data);
+	//2nd state property cart keeps track of whats in cart
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		//step 1
+		setCart([...cart, item])
 	};
 
 	return (
